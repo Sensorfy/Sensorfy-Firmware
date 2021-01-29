54bff167bbb50e76b5fa33e8f84c4f8564c384e1
Marcus Wichelmann <mail@marcusw.de>
2021-01-29 19:28:42 +0100

    Shortened dist paths to upload them to littlefs

--- web/sensorfy-webinterface/vue.config.js
+++ web/sensorfy-webinterface/vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+
+  // Additional plugins
   pluginOptions: {
+    // Compress dist files
     compression: {
       gzip: {
         filename: '[path].gz[query]',
@@ -12,7 +15,65 @@ module.exports = {
       }
     }
   },
+
+  // Output to dist/web so the web/ path is included in the ESP filesystem
+  outputDir: 'dist/web',
+
+  // Webpack options
+  configureWebpack: {
+    // Shorter for js files
+    output: {
+      filename: '[name].[chunkhash:4].js',
+      chunkFilename: '[name].[chunkhash:4].js',
+    },
+
+    // Shorter chunk names
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            name: 'vendors'
+          },
+          common: {
+            name: 'common'
+          }
+        }
+      }
+    }
+  },
+
+  // Shorter for css files
+  css: {
+    extract: {
+      filename: '[name].[chunkhash:4].css',
+      chunkFilename: '[name].[chunkhash:4].css',
+    },
+  },
+
+  // Shorter names for images/fonts/...
+  chainWebpack: config => {
+    config.module.rule('images').use('url-loader').tap(options => {
+      options.fallback.options.name = '[name].[hash:4].[ext]';
+      return options
+    });
+    config.module.rule('svg').use('file-loader').tap(options => {
+      options.name = '[name].[hash:4].[ext]';
+      return options
+    });
+    config.module.rule('media').use('url-loader').tap(options => {
+      options.fallback.options.name = '[name].[hash:4].[ext]';
+      return options
+    });
+    config.module.rule('fonts').use('url-loader').tap(options => {
+      options.fallback.options.name = '[name].[hash:4].[ext]';
+      return options
+    });
+  },
+
+  // Don't put source maps into the dist folder to save space
   productionSourceMap: false,
+
+  // Allow reaching the dev server using local hostnames
   devServer: {
     disableHostCheck: true
   }
